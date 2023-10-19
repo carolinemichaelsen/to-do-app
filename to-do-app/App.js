369b03a8f5f14afdd3ec670911a563a09e75fc27
Caroline Michaelsen <139086533+carolinemichaelsen@users.noreply.github.com>
2023-10-19 19:40:04 -0300

    estrutura e estilização inicial

--- to-do-app/App.js
+++ to-do-app/App.js
@@ -4,8 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Tarefas de Hoje</Text>
+          <View styke={styles.items}>
+
+          </View>
+
+      </View>
+
     </View>
   );
 }
@@ -13,8 +19,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e8eaed',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: bold,
+  },
+  items: {},
 });
